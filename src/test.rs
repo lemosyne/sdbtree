5c6e8a6478131d8a08a1badb89ab36fe29c47919
Eugene Chou <euchou@ucsc.edu>
2023-10-18 06:52:10 +0000

    passes update/deriving keys (i believe)

--- src/test.rs
+++ src/test.rs
@@ -51,3 +51,62 @@ fn reloading() -> Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn correctness() -> Result<()> {
+    let mut rng = ThreadRng::default();
+    let mut tree = BKeyTree::new(
+        "/tmp/bkeytreedir-correctness",
+        utils::generate_key(&mut rng),
+    )?;
+    // A mix of updates and derives.
+    let key1 = tree.update(1)?;
+    let key2 = tree.update(2)?;
+    let key4 = tree.derive(4)?;
+
+    // Committing should yield the two updated keys.
+    let mut commit = tree.commit();
+    commit.sort();
+    assert_eq!(vec![1, 2], commit);
+
+    // We should be able to derive the previously derived/updated keys.
+    let key1_updated = tree.derive(1)?;
+    let key2_updated = tree.derive(2)?;
+    let key4_rederived = tree.derive(4)?;
+    assert_eq!(key1, key1_updated);
+    assert_eq!(key2, key2_updated);
+    assert_eq!(key4, key4_rederived);
+
+    // Just a derive, no updates.
+    let key11 = tree.derive(11)?;
+
+    // Committing should yield no keys.
+    assert!(tree.commit().is_empty());
+
+    let key15 = tree.update(15)?;
+    let key15_derived = tree.derive(15)?;
+    let key13 = tree.derive(13)?;
+    assert_eq!(key15, key15_derived);
+
+    // Committing should yield the one update key.
+    assert_eq!(vec![15], tree.commit());
+
+    // We can still derive the old stuff.
+    let key11_rederived = tree.derive(11)?;
+    let key13_rederived = tree.derive(13)?;
+    let key15_rederived = tree.derive(15)?;
+    assert_eq!(key11, key11_rederived);
+    assert_eq!(key13, key13_rederived);
+    assert_eq!(key15, key15_rederived);
+
+    // One more check on appending.
+    let key16 = tree.derive(16)?;
+
+    // Committing should yield no keys.
+    assert!(tree.commit().is_empty());
+
+    let key16_rederived = tree.derive(16)?;
+    assert_eq!(key16, key16_rederived);
+
+    Ok(())
+}
